888dfe78f546bbc88ba6325c3a3cc7bd92e62944
Kayla-Ann Hurdle <kayla.ann.hurdle@gmail.com>
2025-09-27 16:22:24 -0500

    updating main

---
+++ Wumpus.java
@@ -0,0 +1,29 @@
+package edu.ilstu;
+
+import java.util.Scanner;
+
+public class Main02
+{
+
+	public static void main(String[] args)
+	{
+		// sets up main variables
+		Scanner input = new Scanner(System.in);
+		char movement;
+		boolean go = true;
+
+		// sets up map
+		MazeMapService wumpusWorld = new MazeMapService();
+		wumpusWorld.resetMap();
+		wumpusWorld.showMap();
+
+		while (go)
+		{
+			System.out.println(wumpusWorld.getOptions());
+			wumpusWorld.setLocation(input.next().charAt(0));
+			wumpusWorld.showMap();
+		}
+
+	}
+
+}
